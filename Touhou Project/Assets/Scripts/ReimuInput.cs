92013fe2bb15df3ff371ea8f1de4de015967e1a0
TommyJacksonMann <40581282+TommyJacksonMann@users.noreply.github.com>
2018-07-13 13:27:16 -0400

    Down Attack

--- Touhou Project/Assets/Scripts/ReimuInput.cs
+++ Touhou Project/Assets/Scripts/ReimuInput.cs
@@ -3,56 +3,71 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;
 
-public class ReimuInput : MonoBehaviour {
+public class ReimuInput : MonoBehaviour
+{
 
-    public bool mPlayerAttacking = false;
-    Animator mAnimator;
+   public bool mPlayerAttacking = false;
+   Animator mAnimator;
 
-    GameObject currentAttackCollider;
-    public GameObject[] hitBoxArray;
+   GameObject currentAttackCollider;
+   public GameObject[] hitBoxArray;
 
 
-	// Use this for initialization
-	void Start () {
-        mAnimator = GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+   // Use this for initialization
+   void Start()
+   {
+      mAnimator = GetComponent<Animator>();
+   }
 
-    public void Attack(bool attack, float directionX, float directionY)
-    {
-        if (!mPlayerAttacking)
-        {
-            if (attack)
+   // Update is called once per frame
+   void LateUpdate()
+   {
+      GetComponent<PlayerMotor>().mCanMove = !mPlayerAttacking;
+   }
+
+   public void Attack(bool attack, float directionX, float directionY)
+   {
+      if (!mPlayerAttacking)
+      {
+         if (attack)
+         {
+            mPlayerAttacking = true;
+            Debug.Log("X:" + directionX + "Y:" + directionY);
+            if (directionX == 0 && directionY == 0)
             {
-                
-                Debug.Log("X:" + directionX + "Y:" + directionY);
-                if(directionX == 0 && directionY == 0)
-                {                    
-                    mAnimator.SetTrigger("NeutralAttack");
-                }
+               mAnimator.SetTrigger("NeutralAttack");
+
             }
-        }
-    }
+            else if (directionX > 0.9 || directionX < -0.9)
+            {
+               mAnimator.SetTrigger("SideAttack");
+            }
+            else if (directionY < 0.9)
+            {
+               mAnimator.SetTrigger("DownAttack");
+            }
+            else
+            {
+               SwitchAttackBool();
+            }
+         }
+      }
+   }
 
-    public void SwitchAttackBool()
-    {
-        mPlayerAttacking = !mPlayerAttacking;
-        GetComponent<PlayerMotor>().mCanMove = !GetComponent<PlayerMotor>().mCanMove;
-    }
+   public void SwitchAttackBool()
+   {
+      mPlayerAttacking = false;
+   }
 
-    public void SpawnAttackCollider(int index)
-    {
-        currentAttackCollider = (GameObject)Instantiate(hitBoxArray[index], gameObject.transform);
-        Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), currentAttackCollider.GetComponent<Collider2D>());
-    }
+   public void SpawnAttackCollider(int index)
+   {
+      currentAttackCollider = (GameObject)Instantiate(hitBoxArray[index], gameObject.transform);
+      Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), currentAttackCollider.GetComponent<Collider2D>());
+   }
 
-    public void DestroyAttackCollider()
-    {
-        Destroy(currentAttackCollider);
-    }
+   public void DestroyAttackCollider()
+   {
+      Destroy(currentAttackCollider);
+   }
 
 }
