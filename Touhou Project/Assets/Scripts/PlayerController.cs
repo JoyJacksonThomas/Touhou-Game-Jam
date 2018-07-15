7c943f93dba892e81221b766dcf7333640cb5aa1
361rjb <361rjb@gmail.com>
2018-07-14 20:10:55 -0400

    Pause Kinda Works, 321, and score reset

--- Touhou Project/Assets/Scripts/PlayerController.cs
+++ Touhou Project/Assets/Scripts/PlayerController.cs
@@ -5,62 +5,89 @@ using UnityEngine.Animations;
 
 public class PlayerController : MonoBehaviour
 {
-   int mPlayerIndex;
+    int mPlayerIndex;
 
-   string[] prefix = { "P1 ", "P2 " };
-   private Animator mPlayerAnimator;
-   private PlayerMotor mPlayerMotor;
+    string[] prefix = { "P1 ", "P2 " };
+    private Animator mPlayerAnimator;
+    private PlayerMotor mPlayerMotor;
 
-   public bool mCanInput = true;
+    public bool mCanInput = true;
 
-   void Start()
-   {
-      mPlayerMotor = GetComponent<PlayerMotor>();
-      mPlayerAnimator = GetComponent<Animator>();
-      if (name == "Player1")
-      {
-         mPlayerIndex = 0;
-      }
-      else if (name == "Player2")
-      {
-         mPlayerIndex = 1;
-      }
-   }
+    bool mGamePaused = false;
 
-   void Update()
-   {
-      if(mCanInput)
-      {
-         float _horizontal = Input.GetAxis(prefix[mPlayerIndex] + "Horizontal");
-         mPlayerMotor.Move(_horizontal);
+    void Start()
+    {
+        mPlayerMotor = GetComponent<PlayerMotor>();
+        mPlayerAnimator = GetComponent<Animator>();
+        if (name == "Player1")
+        {
+            mPlayerIndex = 0;
+        }
+        else if (name == "Player2")
+        {
+            mPlayerIndex = 1;
+        }
+    }
 
-         mPlayerAnimator.SetFloat("Horizontal", Mathf.Abs(_horizontal));
+    void Update()
+    {
+        if (mCanInput)
+        {
+            bool _pause = Input.GetButtonDown(prefix[mPlayerIndex] + "Pause");
 
-         float _vertical = Input.GetAxis(prefix[mPlayerIndex] + "Vertical");
+            if (_pause)
+            {
+                Debug.Log("Paused");
+                if (Time.timeScale == 0.0f && mGamePaused)
+                {
+                    mGamePaused = false;
+                    Time.timeScale = 1.0f;
+                }
+                else
+                {
+                    mGamePaused = true;
+                    Time.timeScale = 0.0f;
+                }
 
-         mPlayerMotor.FastFall(_vertical);
+            }
+           
+                float _horizontal = Input.GetAxis(prefix[mPlayerIndex] + "Horizontal");
+                
+
+               
+
+                float _vertical = Input.GetAxis(prefix[mPlayerIndex] + "Vertical");
+
+                mPlayerMotor.FastFall(_vertical);
 
 
 
-         bool _jump = Input.GetButtonDown(prefix[mPlayerIndex] + "Jump");
+                bool _jump = Input.GetButtonDown(prefix[mPlayerIndex] + "Jump");
 
-         bool _attack = Input.GetButtonDown(prefix[mPlayerIndex] + "Attack");
+                bool _attack = Input.GetButtonDown(prefix[mPlayerIndex] + "Attack");
 
-         bool _special = Input.GetButtonDown(prefix[mPlayerIndex] + "Special");
+                bool _special = Input.GetButtonDown(prefix[mPlayerIndex] + "Special");
 
-         if (gameObject.GetComponent<ReimuInput>() != null)
-         {
-            gameObject.GetComponent<ReimuInput>().Attack(_attack, _special, _horizontal, _vertical);
-         }
-         else if (gameObject.GetComponent<MarisaInput>() != null)
-         {
-            gameObject.GetComponent<MarisaInput>().Attack(_attack, _special, _horizontal, _vertical);
-         }
 
-         if (_jump)
-         {
-            mPlayerMotor.Jump();
-         }
-      }
-   }
+            if (Time.timeScale != 0.0f)
+            {
+                mPlayerMotor.Move(_horizontal);
+                mPlayerAnimator.SetFloat("Horizontal", Mathf.Abs(_horizontal));
+
+                if (gameObject.GetComponent<ReimuInput>() != null)
+                {
+                    gameObject.GetComponent<ReimuInput>().Attack(_attack, _special, _horizontal, _vertical);
+                }
+                else if (gameObject.GetComponent<MarisaInput>() != null)
+                {
+                    gameObject.GetComponent<MarisaInput>().Attack(_attack, _special, _horizontal, _vertical);
+                }
+
+                if (_jump)
+                {
+                    mPlayerMotor.Jump();
+                }
+            }
+        }
+    }
 }
--- Touhou Project/Assets/Scripts/SmashBrosCam.cs
+++ Touhou Project/Assets/Scripts/SmashBrosCam.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SmashBrosCam : MonoBehaviour
 {
@@ -16,6 +17,50 @@ public class SmashBrosCam : MonoBehaviour
 
    public float lerpSpeed;
 
+    int mPlayerOneScore = 0;
+    int mPlayerTwoScore= 0;
+
+    Text mPlayerScoreText;
+    Text mCountdownText;
+
+    public float mMinSize = 5;
+    public float mMaxSize = 8;
+    public float mResizeSpeed = .1f;
+    public float mMaxDistance = 5f;
+
+    void Start()
+    {
+        mPlayerScoreText = GameObject.Find("PlayerScoreText").GetComponent<Text>();
+        mPlayerScoreText.gameObject.SetActive(false);
+        mCountdownText = GameObject.Find("Countdown").GetComponent<Text>();
+        StartCoroutine(Countdown());
+
+    }
+    IEnumerator Countdown()
+    {
+        mCountdownText.text = "3";
+        for (int i = 0; i < mTargets.Count; i++)
+        {
+            mTargets[i].GetComponent<PlayerController>().mCanInput = false;
+        }
+
+        yield return new WaitForSeconds(1.0f);
+        mCountdownText.text = "2";
+
+        yield return new WaitForSeconds(1.0f);
+        mCountdownText.text = "1";
+
+        yield return new WaitForSeconds(1.0f);
+        mCountdownText.text = "GO!";
+        for (int i = 0; i < mTargets.Count; i++)
+        {
+            mTargets[i].GetComponent<PlayerController>().mCanInput = true;
+        }
+        yield return new WaitForSeconds(0.5f);
+        mCountdownText.gameObject.SetActive(false);
+    }
+
+
    void LateUpdate()
    {
       Vector3 centerPoint = GetCenterPoint();
@@ -25,9 +70,16 @@ public class SmashBrosCam : MonoBehaviour
       centerPoint.y = Mathf.Clamp(centerPoint.y, yMin, yMax);
 
       transform.position = Vector3.Lerp(transform.position, centerPoint, lerpSpeed);
-
-      SpawnPlayer();
-   }
+        if ((mTargets[1].position - mTargets[0].position).magnitude >= mMaxDistance)
+        {
+            GetComponent<Camera>().orthographicSize = Mathf.Lerp(GetComponent<Camera>().orthographicSize, mMaxSize, mResizeSpeed);
+        }
+        else if ((mTargets[1].position - mTargets[0].position).magnitude < mMaxDistance)
+        {
+            GetComponent<Camera>().orthographicSize = Mathf.Lerp(GetComponent<Camera>().orthographicSize, mMinSize, mResizeSpeed);
+        }
+        //SpawnPlayer();
+    }
 
    Vector3 GetCenterPoint()
    {
@@ -52,4 +104,64 @@ public class SmashBrosCam : MonoBehaviour
          }
       }
    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player1")
+        {
+           /* collision.gameObject.transform.position = Vector3.zero;
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
+            collision.gameObject.GetComponent<PlayerMotor>().mDamageMultiplier = 0;*/
+            mPlayerTwoScore++;
+            ResetPlayers();
+        }
+        else if (collision.gameObject.name == "Player2")
+        {
+           /* collision.gameObject.transform.position = Vector3.zero;
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
+            collision.gameObject.GetComponent<PlayerMotor>().mDamageMultiplier = 0;*/
+            mPlayerOneScore++;
+            ResetPlayers();
+        }
+
+    }
+
+    void ResetPlayers()
+    {
+        for (int i = 0; i < mTargets.Count; i++)
+        {
+            
+            switch(i)
+            {
+                case 0:
+                    mTargets[i].position = new Vector2(i-1f, 0f);
+                    break;
+                case 1:
+                    mTargets[i].position = new Vector2(i + 1f, 0f);
+                    break;
+            }
+            mTargets[i].GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
+            mTargets[i].GetComponent<PlayerMotor>().mDamageMultiplier = 0;
+          
+        }
+        StartCoroutine(ScoreShow());
+
+    }
+
+    IEnumerator ScoreShow()
+    {
+        for (int i = 0; i < mTargets.Count; i++)
+        {
+            mTargets[i].GetComponent<PlayerController>().mCanInput = false;
+        }
+            mPlayerScoreText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        mPlayerScoreText.text = mPlayerOneScore + " - " + mPlayerTwoScore;
+        yield return new WaitForSeconds(2f);
+        for (int i = 0; i < mTargets.Count; i++)
+        {
+            mTargets[i].GetComponent<PlayerController>().mCanInput = true;
+        }
+        mPlayerScoreText.gameObject.SetActive(false);
+    }
 }
